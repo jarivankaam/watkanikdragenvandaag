4dd9a5f1b1340a019ae66296c3a820c8435326b7
jarivankaam <71122219+jarivankaam@users.noreply.github.com>
2022-09-29 16:10:19 +0200

    location test

---
+++ distro/weatherapi.js
@@ -0,0 +1,7 @@
+"use strict";
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+        let jsonurl = "https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=a67d3f36cd740475df0923cbcb7f607c";
+        console.log(jsonurl);
+    });
+}
