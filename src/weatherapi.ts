2f4a62aa3f9b496b275f815e4c3e28c2cdddaca7
jarivankaam <71122219+jarivankaam@users.noreply.github.com>
2022-09-29 16:16:18 +0200

    test

--- src/weatherapi.ts
+++ src/weatherapi.ts
@@ -3,6 +3,8 @@ if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(position => {
         let jsonurl = "https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=a67d3f36cd740475df0923cbcb7f607c"
         let response = fetch(jsonurl);
-        console.log(response)
+        let response_decoded  = response.then(response => response.json());
+        console.log(response_decoded);
+
     })
 }
