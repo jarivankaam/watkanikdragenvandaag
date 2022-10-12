9ff8396fa8db9cb771b78724a4866a0ef9ca30bf
jarivankaam <71122219+jarivankaam@users.noreply.github.com>
2022-10-12 19:25:28 +0200

    rest

--- distro/weatherapi.js
+++ distro/weatherapi.js
@@ -1,9 +1,18 @@
-"use strict";
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-        let jsonurl = "https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=a67d3f36cd740475df0923cbcb7f607c";
-        let response = fetch(jsonurl);
-        let response_decoded = response.then(response => response.json());
-        console.log(response_decoded);
+const api_url = "https://api.openweathermap.org/data/2.5/weather?q=amsterdam,&appid=a67d3f36cd740475df0923cbcb7f607c";
+let Temprature;
+function getTemp() {
+    // For now, consider the data is stored on a static `users.json` file
+    return fetch(api_url)
+        // the JSON body is taken from the response
+        .then(res => res.json())
+        .then(res => {
+        // The response has an `any` type, so we need to cast
+        // it to the `User` type, and return it from the promise
+        return res.main.temp;
     });
 }
+getTemp()
+    .then(temps => {
+    const temp_celcius = temps - 273.15;
+    console.log(temp_celcius);
+});
--- src/weatherapi.ts
+++ src/weatherapi.ts
@@ -1,10 +1,21 @@
+const api_url = "https://api.openweathermap.org/data/2.5/weather?q=amsterdam,&appid=a67d3f36cd740475df0923cbcb7f607c";
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position => {
-        let jsonurl = "https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=a67d3f36cd740475df0923cbcb7f607c"
-        let response = fetch(jsonurl);
-        let response_decoded  = response.then(response => response.json());
-        console.log(response_decoded);
+let Temprature: number;
+function getTemp(): Promise <number> {
+    // For now, consider the data is stored on a static `users.json` file
+    return fetch(api_url)
+    // the JSON body is taken from the response
+    .then(res => res.json())
+    .then(res => {
+        // The response has an `any` type, so we need to cast
+        // it to the `User` type, and return it from the promise
+        return res.main.temp
 
     })
 }
+
+getTemp()
+.then(temps => {
+    const temp_celcius: number = temps - 273.15;
+    console.log(temp_celcius);
+})
