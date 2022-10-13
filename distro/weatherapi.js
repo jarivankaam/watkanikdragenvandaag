da61a7ba2af55ca18eddd61446f838cff8d6a3a8
jarivankaam <71122219+jarivankaam@users.noreply.github.com>
2022-10-13 09:51:40 +0200

    added html and clothes code

--- distro/weatherapi.js
+++ distro/weatherapi.js
@@ -1,18 +1,32 @@
 const api_url = "https://api.openweathermap.org/data/2.5/weather?q=amsterdam,&appid=a67d3f36cd740475df0923cbcb7f607c";
 let Temprature;
 function getTemp() {
-    // For now, consider the data is stored on a static `users.json` file
     return fetch(api_url)
-        // the JSON body is taken from the response
         .then(res => res.json())
         .then(res => {
-        // The response has an `any` type, so we need to cast
-        // it to the `User` type, and return it from the promise
         return res.main.temp;
     });
 }
 getTemp()
     .then(temps => {
     const temp_celcius = temps - 273.15;
-    console.log(temp_celcius);
+    const temp_str = temp_celcius.toFixed(0);
+    let html = document.getElementById('temp');
+    let clothes = document.getElementById('clothes');
+    html.innerHTML = "Het is nu: " + "" + temp_str + "°C" + " buiten";
+    if (temp_celcius < 10) {
+        clothes.innerHTML = "Je hebt een jas nodig";
+    }
+    else if (temp_celcius < 20) {
+        clothes.innerHTML = "Je hebt een vest nodig";
+    }
+    else if (temp_celcius < 30) {
+        clothes.innerHTML = "Je hebt een t-shirt nodig";
+    }
+    else if (temp_celcius < 40) {
+        clothes.innerHTML = "Je hebt een zwembroek nodig";
+    }
+    else {
+        clothes.innerHTML = "Ga weg";
+    }
 });
--- node_modules/openweathermap-ts/dist/types/CountryCode.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// ISO 3166 COUNTRY CODES
-Object.defineProperty(exports, "__esModule", { value: true });
