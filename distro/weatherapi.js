b78e8d47ccf6000b87061396248ce5fe7cade2e4
Jari van Kaam <71122219+jarivankaam@users.noreply.github.com>
2022-12-29 15:16:23 +0100

    WKIDV | weatherapi.ts | added geolocation to api call so that it's based on the users location.

--- distro/weatherapi.js
+++ distro/weatherapi.js
@@ -1,41 +1,46 @@
-const api_url = "https://api.openweathermap.org/data/2.5/weather?q=amsterdam,&appid=a67d3f36cd740475df0923cbcb7f607c";
-let Temprature;
-function getTemp() {
-    return fetch(api_url)
-        .then(res => res.json())
-        .then(res => {
-        return res.main.temp;
+navigator.geolocation.getCurrentPosition((position) => {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    console.log('latitude:' + latitude + 'longitude:' + longitude);
+    const api_url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=becb7265debe1d9f5176ce6e543deca5";
+    let Temprature;
+    function getTemp() {
+        return fetch(api_url)
+            .then(res => res.json())
+            .then(res => {
+            return res.main.temp;
+        });
+    }
+    getTemp()
+        .then(temps => {
+        const temp_celcius = temps - 273.15;
+        const temp_str = temp_celcius.toFixed(0);
+        let html = document.getElementById('temp');
+        let clothes = document.getElementById('clothes');
+        let icon = document.getElementById('icon');
+        html.innerHTML = "Het is nu: " + "" + temp_str + "°C" + " buiten";
+        if (temp_celcius < 10) {
+            clothes.innerHTML = "Je kan het beste een jas en warme kledigen dragen.";
+            icon.innerHTML = '<img src="https://img.icons8.com/jacket"/>';
+        }
+        else if (temp_celcius < 15 && temp_celcius > 10) {
+            clothes.innerHTML = "Je kan het beste een vest en wat dikkere kleren dragen.";
+            icon.innerHTML = '<img src="https://img.icons8.com/vest"/>';
+        }
+        else if (temp_celcius < 20 && temp_celcius > 15) {
+            clothes.innerHTML = "Je kant het beste lichte en dunnne kleding dragen maar je mag ook dikkere kleding dragen.";
+            icon.innerHTML = '<img src="https://img.icons8.com/t-shirt"/>';
+        }
+        else if (temp_celcius < 30) {
+            clothes.innerHTML = "Je kan het beste een t-shirt en korte broek dragen en laat de winterjas maar lekker thuis.";
+            icon.innerHTML = '<img src="https://img.icons8.com/t-shirt"/>';
+        }
+        else if (temp_celcius < 40) {
+            clothes.innerHTML = "Je kan het beste een zwembroek aantrekken want face it het is veelste warm";
+            icon.innerHTML = '<img src="https://img.icons8.com/swimwear"/>';
+        }
+        else {
+            clothes.innerHTML = "Ga weg het is veelste koud of veelste warm";
+        }
     });
-}
-getTemp()
-    .then(temps => {
-    const temp_celcius = temps - 273.15;
-    const temp_str = temp_celcius.toFixed(0);
-    let html = document.getElementById('temp');
-    let clothes = document.getElementById('clothes');
-    let icon = document.getElementById('icon');
-    html.innerHTML = "Het is nu: " + "" + temp_str + "°C" + " buiten";
-    if (temp_celcius < 10) {
-        clothes.innerHTML = "Je kan het beste een jas en warme kledigen dragen.";
-        icon.innerHTML = '<img src="https://img.icons8.com/jacket"/>';
-    }
-    else if (temp_celcius < 15 && temp_celcius > 10) {
-        clothes.innerHTML = "Je kan het beste een vest en wat dikkere kleren dragen.";
-        icon.innerHTML = '<img src="https://img.icons8.com/vest"/>';
-    }
-    else if (temp_celcius < 20 && temp_celcius > 15) {
-        clothes.innerHTML = "Je kant het beste lichte en dunnne kleding dragen maar je mag ook dikkere kleding dragen.";
-        icon.innerHTML = '<img src="https://img.icons8.com/t-shirt"/>';
-    }
-    else if (temp_celcius < 30) {
-        clothes.innerHTML = "Je kan het beste een t-shirt en korte broek dragen en laat de winterjas maar lekker thuis.";
-        icon.innerHTML = '<img src="https://img.icons8.com/t-shirt"/>';
-    }
-    else if (temp_celcius < 40) {
-        clothes.innerHTML = "Je kan het beste een zwembroek aantrekken want face it het is veelste warm";
-        icon.innerHTML = '<img src="https://img.icons8.com/swimwear"/>';
-    }
-    else {
-        clothes.innerHTML = "Ga weg het is veelste koud of veelste warm";
-    }
 });
